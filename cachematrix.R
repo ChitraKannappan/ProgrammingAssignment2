d4d884fc15097a2d30ee75c82dbd4b13b81127bd
Chitra Kannappan <chitra.kann@gmail.com>
2016-06-28 15:03:44 -0500

    Committing cacheSolve
    Chitra's cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## 1.set the value of the matrix
 ## 2.get the value of the matrix
 ## 3.set the value of the inverse matrix
-## 4.get the value of the inverse  matrix
+## 4.get the value of the inverse matrix
 
 
 
